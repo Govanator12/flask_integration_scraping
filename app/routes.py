3195c45ef85b2d93904566e8235ffaf8af513b43
Steven Hardy <steverhardy@gmail.com>
2019-05-31 13:34:46 -0400

    Added graph section

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
 from flask import render_template
-from app.parser import getPoem
 from app.data import getStats
+from app.parser import getPoem
+from app.graph import plotPoints
 
 
 @app.route('/')
@@ -9,10 +10,24 @@ from app.data import getStats
 def index():
     url = 'http://www.arthurleej.com/e-love.html'
     response = getPoem(url)
-
     return render_template('index.html', response=response)
 
+
 @app.route('/dataframe')
-def dataFrame():
+def dataframe():
     response = getStats()
     return render_template('dataframe.html', response=response)
+
+
+@app.route('/graph')
+def graph():
+    x = [1, 2, 3, 4]
+    y = [10, 12.2, 18.6, 8.5]
+
+    xlabel = 'X Axis'
+    ylabel = 'Y Axis'
+    title = 'Flask Integrated Graph'
+
+    name = plotPoints(x, y, xlabel, ylabel, title)
+
+    return render_template('graph.html', name=name)
